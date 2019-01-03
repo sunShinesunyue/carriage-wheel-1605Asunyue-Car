0f39a5ab2341465e4bfb40e103c5c2bdc41d03e8
sunyue <814983809>
2019-01-03 19:15:42 +0800

    弹出层 six

--- car/src/api/index.js
+++ car/src/api/index.js
@@ -1,5 +1,31 @@
 import JSBridge from '@/utils/JSBridge.js';
 
+function sendRequest(url,method = 'GET',data = {}) {
+    let params = {
+        method
+    };
+
+    // 判断如果是一个post请求,带上请求体信息
+    if(method == 'POST') {
+        params.body = JSON.stringify(data);
+    }
+
+    // 判断请求查询url是否勋携带query
+    if(url.indexOf('?') == -1) {
+        url += `?_=${+new Date()}`
+    } else {
+        url += `&_=${new Date()}`
+    }
+
+    //  拼接登录态token
+    // url += `&token=${getToken()}`;
+    return fetch(url,params)
+    .then(res => res.json())
+    .then(body => body)
+}
+
+
+// 上传图片
 export let upLoadImg = (type) => {
     return new Promise((resolve,reject) => {
         JSBridge.invoke('device','chooseImage',{
