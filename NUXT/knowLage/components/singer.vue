b1937ca205c058cb6e7753d585a812c8e4ddb4b5
sunyue <814983809>
2019-01-07 10:11:00 +0800

    animation动画

--- NUXT/knowLage/components/singer.vue
+++ NUXT/knowLage/components/singer.vue
@@ -1,6 +1,6 @@
 <template>
     <div class='singerResult'>
-        <img src='../static/man.jpg'>
+        <img src='../static/man.jpg' class='singerImage'>
     </div>
 </template>
 
@@ -9,16 +9,22 @@
 
 <style>
     .singerResult {
-        animation: goright 0.5s linear forwards 3.5s;
+        animation: role 2s ease-in-out 1s infinite alternate;
     }
-    @keyframes godown {
-        30% {
-            top: 180px;
-        }
-    }
-    @keyframes goright {
-        100% {
-            left: 70px;
-        }
-    }
+    @keyframes role {
+  0% {
+    transform: rotateX(180deg);
+    background: #000;
+  }
+   50% {
+    transform: rotateX(360deg);
+    background: red;
+  }
+  50% {
+    transform: rotateY(360deg);
+    background: red;
+  }
+}
+
+
 </style>
